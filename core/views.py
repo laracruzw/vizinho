2678e9c0fb7cdde45c8da9dc230728fb6fe69afa
Lara da Cruz <laradacruzw@Laras-MacBook-Air.local>
2026-07-14 15:32:30 -0300

    teste

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 def home(request):
-    return HttpResponse("Olá, esta é a home do Vizinho de Aluguel!")
+    return render(request, "core/index.html")
 
 def sobre(request):
     return HttpResponse("Página sobre o projeto.")
