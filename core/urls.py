fcf8043cc64dd9a3979504e646e5ed06ed3c6f67
Lara da Cruz <laradacruzw@Laras-MacBook-Air.local>
2026-07-14 10:37:39 -0300

    comentários ainda dia 3

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("sobre/", views.sobre, name="sobre"),
+    path("", views.home, name="home"), # "": endereço, views.home é a funct que responde,
+    path("sobre/", views.sobre, name="sobre"), #  name é o apelido pra essa rota
 ]
