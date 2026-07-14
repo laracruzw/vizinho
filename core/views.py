39080b41a8f41abe3d3fa39deed98cd952250e2a
Lara da Cruz <laradacruzw@Laras-MacBook-Air.local>
2026-07-14 15:44:16 -0300

    teste

--- core/views.py
+++ core/views.py
@@ -2,7 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
 def home(request):
-    return render(request, "core/index.html")
+    demandas = [
+        {"titulo": "Trocar tomada queimada", "categoria": "Elétrica", "cidade": "Florianópolis"},
+        {"titulo": "Pintar quarto pequeno", "categoria": "Pintura", "cidade": "São José"},
+        {"titulo": "Consertar vazamento na pia", "categoria": "Hidráulica", "cidade": "Palhoça"},
+    ]
+    return render(request, "core/index.html", {"demandas": demandas})
 
 def sobre(request):
     return HttpResponse("Página sobre o projeto.")
